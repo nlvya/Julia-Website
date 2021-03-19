66dfc97af9ef0e5fb7eac21b74e3de8d45ff8d79
Nlvya <minecraftengine10@gmail.com>
2021-03-18 18:42:18 -0700

    finished gomepage

--- js/index.js
+++ js/index.js
@@ -1,26 +1,6 @@
-function startScrolling(x) {
-    for (let i = 1; i <= x; i++) {
-        let speed = Math.floor(Math.random() * (10 - 5) ) + 5;
-        scroll(i, speed);
-    }
-}
-
-
-const timer = ms => new Promise(res => setTimeout(res, ms)) //creates a function to delay each iteration of the movements
-async function scroll(x, y) {
-    var rate = 10;
-
-    var slideshow = document.getElementById('slide' + x);
-    var height = (slideshow.scrollHeight - slideshow.offsetHeight);
-    for (let i = 0; i <= height + 1; i ++) {
-        var speed = y;
-        slideshow.scroll(0,rate);
-        rate ++;
-        if (i >= height) {
-            i = 0;
-            rate = 10;
-            slideshow.scroll(0,36);
-        }
-        await timer(speed);
+function moveSlides() {
+    for (let i = 1; i <= 4; i++) {
+        var elem = document.getElementById('slideImg' + i);
+        elem.style.transform = "translateY(calc(100vh - 100%))";
     }
 }
